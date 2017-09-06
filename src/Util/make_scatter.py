f450af4bc3ee23fa92326da5494ce5e45a09acad
Artur <ar-2@tut.by>
2017-09-06 16:48:18 +0300

    rename variables, add vline plot to fleets and to time of games

--- src/Util/make_scatter.py
+++ src/Util/make_scatter.py
@@ -11,22 +11,36 @@ bot1 = "Dave2Player"
 
 if __name__ == '__main__':
     file = open(sys.argv[1]).read()
-    x = []
-    y1 = []
-    y2 = []
+    game_num = []
+    fleet_b1 = []
+    fleet_b2 = [] 
+    game_time = []
+
     lines = file.split("\n")
     for line in lines:
         tokens = line.split(':')
         if len(tokens) <3:
             break
-        x.append(int(tokens[0]))
-        y1.append(int(tokens[4].split(',')[1]))
-        y2.append(int(tokens[5].split(',')[1]))
-        y2 = map(lambda i: -i,y2)
+        game_num.append(int(tokens[0]))
+        if tokens[2] == bot1:
+            game_time.append(int(tokens[3]))
+        else:
+            game_time.append(-int(tokens[3]))
+        fleet_b1.append(int(tokens[4].split(',')[1]))
+        fleet_b2.append(int(tokens[5].split(',')[1]))
+    fleet_b2 = map(lambda i: -i,fleet_b2)
 
    
     #for each point, draw them on a bitmap
-    plt.bar(x, y1, facecolor='#9999ff', edgecolor='white')
-    plt.bar(x, y2, facecolor='#ff9999', edgecolor='white')
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.grid(True)
+    ax1.vlines(game_num, [0], fleet_b1, colors='#9999ff')
+
+    
+    ax1.vlines(game_num, [0], fleet_b2, color='#ff9999')   
+
+    ax2 = fig.add_subplot(212)
+    ax2.vlines(game_num, [0], game_time)   
     plt.savefig('../scatter.pdf')
     plt.show()
