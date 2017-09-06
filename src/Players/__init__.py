d3324ff773546fe2874fe3558e5e9dee6d9ab406
Artur <ar-2@tut.by>
2017-09-06 16:44:04 +0300

    style-fix

--- src/Players/__init__.py
+++ src/Players/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ["BasePlayer", "BasicPlayer", "ScoutPlayer","Dave2Player", "Dave2Player_old"]
+__all__ = ["BasePlayer", "BasicPlayer", "ScoutPlayer","Dave2Player","Dave2Player_old"]
 
 from BasePlayer import BasePlayer
 from BasicPlayer import BasicPlayer
 from ScoutPlayer import ScoutPlayer
 from Dave2Player import Dave2Player
 from Dave2Player_old import Dave2Player_old
+
